0b70e17ffef3de620fd6a846a00e3a59ef21ae07
stablestud <stablestud@protonmail.com>
2018-04-11 20:47:06 +0200

    Updated shellcheck.sh to check for the new scripts

--- test/shellcheck.sh
+++ test/shellcheck.sh
@@ -12,7 +12,7 @@ fi
 echo "Running shellcheck ..."
 
 shellcheck -e SC2154 -e SC1090 -e SC2163 \
-        "${source_dir_path}/../src/adsorber" \
+        "${source_dir_path}/../src/bin/adsorber" \
         "${source_dir_path}/../src/lib/config.sh" \
         "${source_dir_path}/../src/lib/install.sh" \
         "${source_dir_path}/../src/lib/update.sh" \
@@ -20,4 +20,9 @@ shellcheck -e SC2154 -e SC1090 -e SC2163 \
         "${source_dir_path}/../src/lib/restore.sh" \
         "${source_dir_path}/../src/lib/remove.sh" \
         "${source_dir_path}/../src/lib/colours.sh" \
-        "${source_dir_path}/../install.sh"
+        "${source_dir_path}/../src/lib/cron/cron.sh" \
+        "${source_dir_path}/../src/lib/systemd/systemd.sh" \
+        "${source_dir_path}/../adsorber-portable.sh" \
+        "${source_dir_path}/../install-to-system.sh" \
+        "${source_dir_path}/../remove-from-system.sh" \
+        "${source_dir_path}/shellcheck.sh"
