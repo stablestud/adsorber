301753af1d972ee95a53e3d73093856187674f3b
stablestud <stablestud@protonmail.com>
2018-05-24 01:32:07 +0200

    Fixed clean.sh

--- misc/clean.sh
+++ misc/clean.sh
@@ -4,22 +4,22 @@ readonly source_dir_path="$(cd "$(dirname "${0}")"/.. && pwd)"
 
 echo "Cleaning ${source_dir_path} ..."
 
-if [ -f "${source_dir_path}/../adsorber.conf" ]; then
-	rm "${source_dir_path}/../adsorber.conf" -f
+if [ -f "${source_dir_path}/adsorber.conf" ]; then
+	rm "${source_dir_path}/adsorber.conf" -f
 	echo "Removed adsorber.conf"
 fi
 
-if [ -f "${source_dir_path}/../whitelist" ]; then
-	rm "${source_dir_path}/../whitelist" -f
+if [ -f "${source_dir_path}/whitelist" ]; then
+	rm "${source_dir_path}/whitelist" -f
 	echo "Removed whitelist"
 fi
 
-if [ -f "${source_dir_path}/../blacklist" ]; then
-	rm "${source_dir_path}/../blacklist" -f
+if [ -f "${source_dir_path}/blacklist" ]; then
+	rm "${source_dir_path}/blacklist" -f
 	echo "Removed blacklist"
 fi
 
-if [ -f "${source_dir_path}/../sources.list" ]; then
-	rm "${source_dir_path}/../sources.list" -f
+if [ -f "${source_dir_path}/sources.list" ]; then
+	rm "${source_dir_path}/sources.list" -f
 	echo "Removed sources.list"
 fi
