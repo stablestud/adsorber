1c79303806a71cb2e65d547602a224792e506044
stablestud <stablestud@protonmail.com>
2018-05-24 01:28:57 +0200

    Added clean.sh

---
+++ misc/README.md
@@ -0,0 +1,1 @@
+This directory contains a few tools which may aid in the development of Adsorber.
--- misc/clean.sh
+++ misc/clean.sh
@@ -1,27 +1,25 @@
 #!/bin/sh
 
-readonly source_dir_path="$(cd "$(dirname "${0}")" && pwd)"
+readonly source_dir_path="$(cd "$(dirname "${0}")"/.. && pwd)"
 
 echo "Cleaning ${source_dir_path} ..."
 
 if [ -f "${source_dir_path}/../adsorber.conf" ]; then
-	rm "${source_dir_path}/../adsorber.conf"
+	rm "${source_dir_path}/../adsorber.conf" -f
 	echo "Removed adsorber.conf"
 fi
 
 if [ -f "${source_dir_path}/../whitelist" ]; then
-	rm "${source_dir_path}/../whitelist"
+	rm "${source_dir_path}/../whitelist" -f
 	echo "Removed whitelist"
 fi
 
 if [ -f "${source_dir_path}/../blacklist" ]; then
-	rm "${source_dir_path}/../blacklist"
+	rm "${source_dir_path}/../blacklist" -f
 	echo "Removed blacklist"
 fi
 
 if [ -f "${source_dir_path}/../sources.list" ]; then
-	rm "${source_dir_path}/../sources.list"
+	rm "${source_dir_path}/../sources.list" -f
 	echo "Removed sources.list"
 fi
-
-
