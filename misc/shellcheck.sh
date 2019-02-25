575718f0363f2dc4844faf1ed2643a71fdcfbfb0
stablestud <stablestud@protonmail.com>
2019-02-25 19:15:17 +0100

    Fixed illegal option error for cd
    Not every 'cd' command accepts the option -e, therefore I removed it.

--- misc/shellcheck.sh
+++ misc/shellcheck.sh
@@ -8,19 +8,20 @@ readonly script_dir_path="$(cd "$(dirname "${0}")" && pwd)"
 
 if ! command -v shellcheck 2>/dev/null 1>&2; then
         echo "Shellcheck must be installed."
+	exit 1
 fi
 
 echo "Running shellcheck ..."
 
 (
-	cd -P -e "${script_dir_path}" || { echo "Couldn't descend to ${script_dir_path}"; exit 1; }
+	cd -P "${script_dir_path}" || { echo "Couldn't descend to ${script_dir_path}"; exit 1; }
 
         shellcheck -x \
+                "${script_dir_path}/shellcheck.sh" \
                 "${script_dir_path}/../src/bin/adsorber" \
                 "${script_dir_path}/../portable_adsorber.sh" \
-                "${script_dir_path}/../install_to_system.sh" \
-                "${script_dir_path}/../remove_from_system.sh" \
-                "${script_dir_path}/shellcheck.sh"
+                "${script_dir_path}/../place_files_onto_system.sh" \
+                "${script_dir_path}/../remove_from_system.sh"
 
         echo "Done."
 )
